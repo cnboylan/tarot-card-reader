f4e55605506ef9a4b2210b0de355684751e25531
Cat Boylan <cnboylan@gmail.com>
2020-04-27 22:51:13 -0400

    added images, 3rd party api, flip functionality

--- src/components/CardSpread.vue
+++ src/components/CardSpread.vue
@@ -8,151 +8,109 @@
       <button class="button is-primary" @click="tenCards">Ten Cards</button>
     </div>
 
-    <div class="spread" id="spread1" style="display: none">
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
+    <div class="spread" id="spread">
+      <div id="container" v-for="card in drawnCards" :key="card">
+        <div v-show="!flipped" class="card-back" @click="flipCard">
+          <div class="card">
+            <img class="logo" src="../assets/tarot-logo.png" />
+          </div>
         </div>
-      </div>
-    </div>
 
-    <div class="spread" id="spread3" style="display: none">
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
-      </div>
-
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
-      </div>
-
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
-      </div>
-    </div>
-
-    <div class="spread" id="spread10" style="display: none">
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
-      </div>
-
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
-      </div>
-
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
-      </div>
-
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
-      </div>
-
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
-      </div>
-
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
-      </div>
-
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
-      </div>
-
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
-      </div>
-
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
-      </div>
-
-      <div class="card-div">
-        <div class="card">
-          <img class="logo" src="../assets/tarot-logo.png" />
-        </div>
+        <transition name="flip">
+          <div v-show="flipped" class="card-front">
+            <div class="card" id="front">
+              <img class="face" :src='"../assets/img/" + card + ".jpg"' />
+            </div>
+          </div>
+        </transition>
       </div>
     </div>
 
     <div class="clear">
-      <button class="button is-small is-dark">Clear</button>
+      <button class="button is-small is-dark" @click="clearCards">Clear</button>
     </div>
   </div>
 </template>
 
 <script>
-// import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "card-spread",
   data: () => ({
-      cardDivs: [],
-      drawnCards: []
+    cardDivs: [],
+    drawnCards: [],
+    flipped: false
   }),
   methods: {
-      oneCard() {
-          const oneCard = document.getElementById("spread1");
-          const threeCards = document.getElementById("spread3");
-          const tenCards = document.getElementById("spread10");
+    oneCard() {
 
-          oneCard.style.display = "flex";
-          threeCards.style.display = "none";
-          tenCards.style.display = "none";
+      this.flipped = false;
+      this.drawnCards = [];
 
-          // axios.get('https://rws-cards-api.herokuapp.com/api/v1/cards/random')
-          //   .then(response => {
-          //     this.drawnCards = response.data.cards;
-          //   });
-      },
+      axios.get("http://localhost:8080/api/v1/cards/random").then(response => {
+        let reading = response.data.cards;
+        reading.forEach(element => {
+          this.drawnCards.push(element.name_short);
+        });
+      });
+    },
 
-      threeCards() {
-          const oneCard = document.getElementById("spread1");
-          const threeCards = document.getElementById("spread3");
-          const tenCards = document.getElementById("spread10");
+    threeCards() {
 
-          oneCard.style.display = "none";
-          threeCards.style.display = "flex";
-          tenCards.style.display = "none";
-      },
+      this.flipped = false;
+      this.drawnCards = [];
 
-      tenCards() {
-          const oneCard = document.getElementById("spread1");
-          const threeCards = document.getElementById("spread3");
-          const tenCards = document.getElementById("spread10");
+      axios
+        .get("http://localhost:8080/api/v1/cards/random?n=3")
+        .then(response => {
+          let reading = response.data.cards;
+          reading.forEach(element => {
+            this.drawnCards.push(element.name_short);
+          });
+        });
+    },
 
-          oneCard.style.display = "none";
-          threeCards.style.display = "none";
-          tenCards.style.display = "flex";
-      }
+    tenCards() {
+
+      this.flipped = false;
+      this.drawnCards = [];
+
+      axios
+        .get("http://localhost:8080/api/v1/cards/random?n=10")
+        .then(response => {
+          let reading = response.data.cards;
+          reading.forEach(element => {
+            this.drawnCards.push(element.name_short);
+          });
+        });
+    },
+
+    flipCard() {
+      this.flipped = true;
+    },
+
+    clearCards() {
+      this.drawnCards = [];
+    }
   }
 };
 </script>
 
 <style>
+.flip-enter-active {
+  transition: all 0.6s ease;
+}
+
+.flip-enter {
+  transform: rotateY(90deg);
+  opacity: 0;
+}
+
+.flip-leave {
+  display: none;
+}
+
 .main {
   background-image: url(../assets/night-sky.jpg);
   background-size: cover;
@@ -187,18 +145,36 @@ export default {
   justify-content: center;
   align-items: center;
   border-radius: 8%;
+  cursor: pointer;
 }
 
-.card-div {
-    display: flex;
-    background-color: midnightblue;
-    height: 50vh;
-    width: 15vw;
-    justify-content: center;
-    align-items: center;
-    border-radius: 8%;
-    border: solid;
-    border-color: #ebebeb;
+.card-back,
+.card-front {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card-back {
+  background-color: midnightblue;
+  height: 50vh;
+  width: 15vw;
+  border-radius: 8%;
+  border: solid;
+  border-color: #ebebeb;
+}
+
+.card-front {
+  background-color: white;
+  border-radius: 8%;
+  border: solid;
+  border-color: #fff;
+}
+
+.face {
+  border-radius: 12%;
+  height: 48.5vh;
+  width: 14vw;
 }
 
 .clear {
@@ -213,4 +189,7 @@ export default {
   background-color: midnightblue;
 }
 
+.primary {
+  color: #117182;
+}
 </style>
